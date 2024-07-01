7836cf288664d8480f02ab70b092a1e8804906ec
changchanghwang <hch950627@naver.com>
2024-07-01 23:35:46 +0900

    fix: use sync.Map

--- with_framework/database/database.go
+++ with_framework/database/database.go
@@ -13,7 +13,7 @@ type accountEntity struct {
 }
 
 type Database struct {
-	accounts map[string]accountEntity
+	accounts sync.Map
 }
 
 func New() *Database {
@@ -25,62 +25,70 @@ type WhereOption struct {
 }
 
 func (db *Database) Upsert(data *account.Account) error {
-	if db.accounts == nil {
-		db.accounts = make(map[string]accountEntity)
-	}
-
 	mutex := &sync.Mutex{}
-	if existAccount, ok := db.accounts[data.Id]; ok {
-		if existAccount.Account.Id != data.Id && existAccount.Account.UserId == data.UserId {
-			return errors.New("already exist userId")
-		}
-		mutex = existAccount.Mutex
-	}
-	db.accounts[data.Id] = accountEntity{data, mutex}
 
+	if existAccount, isExist := db.accounts.Load(data.Id); isExist {
+		account := existAccount.(accountEntity)
+		mutex = account.Mutex
+	}
+
+	isUniqueExist := false
+
+	db.accounts.Range(func(key, value interface{}) bool {
+		account := value.(accountEntity)
+
+		if account.Account.UserId == data.UserId {
+			isUniqueExist = true
+			return false
+		}
+
+		return true
+	})
+
+	if isUniqueExist {
+		return errors.New("already exist userId")
+	}
+
+	db.accounts.Store(data.Id, accountEntity{Account: data, Mutex: mutex})
 	return nil
 }
 
 func (db *Database) Select(data account.Account, where any) ([]*account.Account, error) {
 	accounts := make([]*account.Account, 0)
 	whereOption, isWhereExist := where.(WhereOption)
-
-	if isWhereExist {
-		for _, account := range db.accounts {
+	db.accounts.Range(func(key, value interface{}) bool {
+		account := value.(accountEntity)
+		if isWhereExist {
 			userId, isString := whereOption.UserId.(string)
 			if isString && account.Account.UserId == userId {
 				accounts = append(accounts, account.Account)
 			}
+		} else {
+			accounts = append(accounts, account.Account)
 		}
-		return accounts, nil
-	} else {
-		// where절이 없으면 비동기로 map을 돌면서 slice로 만들어서 반환한다.
-		var wg sync.WaitGroup
-		wg.Add(len(db.accounts))
-
-		for _, account := range db.accounts {
-			go func() {
-				defer wg.Done()
-				accounts = append(accounts, account.Account)
-			}()
-		}
-		wg.Wait()
-		return accounts, nil
-	}
+		return true
+	})
+	return accounts, nil
 }
 
 func (db *Database) Lock(where WhereOption) {
-	for _, account := range db.accounts {
-		if account.Account.UserId == where.UserId {
-			account.Lock()
+	db.accounts.Range(func(key, value interface{}) bool {
+		accountEntity := value.(accountEntity)
+		if accountEntity.Account.UserId == where.UserId {
+			accountEntity.Lock()
+			return false
 		}
-	}
+		return true
+	})
 }
 
 func (db *Database) Unlock(where WhereOption) {
-	for _, account := range db.accounts {
-		if account.Account.UserId == where.UserId {
-			account.Unlock()
+	db.accounts.Range(func(key, value interface{}) bool {
+		accountEntity := value.(accountEntity)
+		if accountEntity.Account.UserId == where.UserId {
+			accountEntity.Unlock()
+			return false
 		}
-	}
+		return true
+	})
 }
